79c5426a97dc710a3eb6f23c56a7cc9f9fc94aba
Rodrigo Pedroza <rodrigobeavis@yahoo.com.br>
2018-04-30 17:34:33 -0300

    aula 11

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url : string = 'http://loaine.traning';
+  cursoAngular:Boolean = true;
+  // urlImagen:string = "http://lorempixel.com/g/400/200/";
+  urlImagen : string = 'http://via.placeholder.com/400x200';
+
+  valorAual: string = '';
+  valorSalvo: string = '';
+
+  isMouseOver: boolean = false;
+
+
+  constructor() { }
+
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+    console.log(this.isMouseOver);
+  }
+
+botaoClicado(){
+  alert('clique no botão');
+}
+
+onKeyUP(event:KeyboardEvent){
+  this.valorAual = ((<HTMLInputElement>event.target).value);
+}
+
+salvarValor(valor){
+  this.valorSalvo = valor;
+}
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  ngOnInit() {
+  }
+
+}
