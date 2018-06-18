1d725e34189463f305bca58a562d617a59ec95a9
Desenvolvimento <desenvolvimento@iperev.com.br>
2018-06-18 08:34:39 -0300

    aula 40

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
+//import { CriarCursoComponent } from './criar-curso/criar-curso.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CursosComponent
+    CursosComponent    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CriarCursoModule
   ],
-  providers: [CursosService],
+  //providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -3,11 +3,19 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class CursosService{
+    
+
+    private cursos: string[] =   ['Angular 2', 'Java', 'Phonegap'];;
    
-
+constructor(){
+        console.log('CursosService');
+    }
     getCursos(){
+        return this.cursos;
 
-        return ['Angular 2', 'Java', 'Phonegap'];
+    }
 
+    addCurso(curso: string){
+        this.cursos.push(curso);
     }
 }
