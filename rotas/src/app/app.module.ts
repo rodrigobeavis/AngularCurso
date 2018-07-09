22ea3d3bcd5d65d3790421d678da242861eb6438
Desenvolvimento <desenvolvimento@iperev.com.br>
2018-07-09 07:55:59 -0300

    Aula 70

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FormsModule } from '@angular/forms';
 import { AuthGuard } from './guards/auth.guard';
 import { CursosGuard } from './guards/cursos.guard';
 import { AlunosGuard } from './guards/aunos.guard';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 // import { CursosModule } from './cursos/cursos.module';
 // import { AlunosModule } from './alunos/alunos.module';
@@ -28,6 +29,7 @@ import { AlunosGuard } from './guards/aunos.guard';
     AppComponent,
     HomeComponent,
     LoginComponent,
+    PaginaNaoEncontradaComponent,
     
     // AlunosComponent,
     // CursosComponent,
--- rotas/src/app/app.routing.module.ts
+++ rotas/src/app/app.routing.module.ts
@@ -1,4 +1,5 @@
-import { NgModule } from '@angular/core';
+
+import { NgModule, Component } from '@angular/core';
 import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -9,34 +10,45 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './guards/auth.guard';
 import { CursosGuard } from './guards/cursos.guard';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 const appRoutes: Routes = [
-  {path: 'cursos', 
+  {
+    path: 'cursos',
     loadChildren: 'app/cursos/cursos.module#CursosModule',
     canActivate: [AuthGuard],
     canActivateChild: [CursosGuard],
     canLoad: [AuthGuard]
   },
-    
-  {path: 'alunos', 
+
+  {
+    path: 'alunos',
     loadChildren: 'app/alunos/alunos.module#AlunosModule',
     canActivate: [AuthGuard],
     canLoad: [AuthGuard]
-   // canActivateChild: [AlunosGuard]
+    // canActivateChild: [AlunosGuard]
   },
   // { path: 'cursos', component: CursosComponent },
   // { path: 'curso/:id', component: CursoDetalheComponent },
   // { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
   { path: 'login', component: LoginComponent },
-  { path: '', 
-             component: HomeComponent,
-             canActivate: [AuthGuard]
-            }
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+   redirectTo: '/home',
+   pathMatch: 'full'
+  },
+  { path: '**', component: PaginaNaoEncontradaComponent }
+
 ];
 
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
