efa466e500d19b6480ae4a2f2067d962d90a6f62
Desenvolvimento <desenvolvimento@ieprev.com.br>
2018-08-01 07:57:23 -0300

    aula 98

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -11,35 +11,40 @@ import { map } from 'rxjs/operators';
 export class DataFormComponent implements OnInit {
 
   // Objeto que vai representar o form no DOM Formulario
-    formulario: FormGroup;
+  formulario: FormGroup;
 
   constructor(private formBuilder: FormBuilder,
-                  private http: Http) { }
+    private http: Http) { }
 
   ngOnInit() {
 
     // this.formulario = new FormGroup({
     //   nome: new FormControl(null),
-    //   email: new FormControl(null)
+    //   email: new FormControl(null),
+    // endereco: new FormGroup({
+    //   cep: new FormControl(),
+    // });
     // });
 
     this.formulario = this.formBuilder.group({
-        nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-        email: [null, [Validators.required, Validators.email]],
+      nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+      email: [null, [Validators.required, Validators.email]],
+      endereco: this.formBuilder.group({
         cep: [null, Validators.required],
         numero: [null, Validators.required],
         complemento: [null],
         logradouro: [null, Validators.required],
         bairro: [null, Validators.required],
         cidade: [null, Validators.required],
-        estado : [null, Validators.required]
+        estado: [null, Validators.required]
+      })
     });
 
   }
 
   verificaValidTouched(campo: string) {
-    return !this.formulario.get(campo).valid  && this.formulario.get(campo).touched;
-  //  return !campo.valid && campo.touched; // template
+    return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
+    //  return !campo.valid && campo.touched; // template
   }
 
   verificaEmailInvalido() {
@@ -58,8 +63,8 @@ export class DataFormComponent implements OnInit {
   }
 
   onSubmit() {
-      console.log(this.formulario);
-      this.http.post('https://httpbin.org/post' , JSON.stringify(this.formulario.value))
+    console.log(this.formulario);
+    this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
       .pipe(map(rst => rst))
       .subscribe(dados => {
         console.log(dados);
@@ -67,10 +72,75 @@ export class DataFormComponent implements OnInit {
         // this.formulario.reset();
         this.resetar();
       }, (error: any) => alert('error'));
-      }
+  }
 
 
-      resetar() {
-        this.formulario.reset();
+  resetar() {
+    this.formulario.reset();
+  }
+
+
+  
+  populaDadosForm(dados) {
+    // formulario.setValue({
+    //   'nome': formulario.value.nome,
+    //   'email': formulario.value.email,
+    //   'endereco': {
+    //     'cep': dados.cep,
+    //     'logradouro': dados.logradouro,
+    //     'numero': '',
+    //     'complemento': dados.complemento,
+    //     'bairro': dados.bairro,
+    //     'cidade': dados.localidade,
+    //     'estado ': dados.uf
+    //   }
+    // });
+
+    this.formulario.patchValue({
+      'endereco': {
+        'cep': dados.cep,
+        'logradouro': dados.logradouro,
+        'numero': '',
+        'complemento': dados.complemento,
+        'bairro': dados.bairro,
+        'cidade': dados.localidade,
+        'estado ': dados.uf
       }
+    });
+
+  }
+
+
+  resetForm() {
+    this.formulario.patchValue({
+      'endereco': {
+        'logradouro': null,
+        'numero': null,
+        'complemento': null,
+        'bairro': null,
+        'cidade': null,
+        'estado ': null
+      }
+    });
+  }
+
+
+  consultaCEP() {
+    let cep = this.formulario.get('endereco.cep').value;
+
+    cep = cep.replace(/\D/g, '');
+
+    if (cep !== '') {
+
+      const validacep = /^[0-9]{8}$/;
+
+      if (validacep.test(cep)) {
+        this.resetForm();
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+          .pipe(map(dados => dados.json()))
+          .subscribe(dados => this.populaDadosForm(dados));
+
+      }
+    }
+  }
 }
