4df8f5dc2d2c393693f8dfc555986687f11773f4
Desenvolvimento <desenvolvimento@ieprev.com.br>
2019-01-15 09:19:43 -0200

    update

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import{MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component'
 import { CursosModule } from './cursos/cursos.module';
+import { ContagemTempoConclusaoComponent } from './contagem-tempo-conclusao/contagem-tempo-conclusao.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiroComponent,
-    MeuPrimeiro2Component
+    MeuPrimeiro2Component,
+    ContagemTempoConclusaoComponent
   ],
   imports: [
     BrowserModule,
